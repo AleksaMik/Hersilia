ac08e2016f75ff78b656b37e7886a2460606899c
AleksaMik <aleksam.2509@gmail.com>
2022-08-24 16:57:23 -0500

    server.js Routes added

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const path = require ('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require ('./routes.htmlRoutes');
 
 app.use(express.urlencoded({
     extended:true
 }));
 app.use(express.json());
-
 app.use(express.static('public'));
+app.use('api', apiRoutes);
+app.use('/', htmlRoutes);
 
 
 app.listen(PORT, () => {
