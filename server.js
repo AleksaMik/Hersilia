8627da96fa50931ed504856abe69f6af4cfe4194
AleksaMik <aleksam.2509@gmail.com>
2022-08-25 20:58:52 -0500

    .gitignore moved

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require ('express');
-const fs = require ('fs');
-const path = require ('path');
-const app = express();
 const PORT = process.env.PORT || 3001;
-
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
 const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require ('./routes/htmlRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.urlencoded({
     extended: true
 }));
-app.use(express.json());
+
 app.use(express.static('public'));
-app.use('api', apiRoutes);
+app.use(express.json());
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 
 app.listen(PORT, () => {
-    console.log(`listen on port ${PORT}`)
+    console.log(`API server now on port ${PORT}!`);
 });
