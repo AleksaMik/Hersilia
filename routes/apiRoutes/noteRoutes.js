132bab79f51177803d13822182eac19f9a9c9a99
AleksaMik <aleksam.2509@gmail.com>
2022-08-25 20:52:12 -0500

    delete routes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -3,15 +3,26 @@ const {
     notes
 } = require('../../db/db');
 const {
-    newNote
-} = require('../../lib/note');
+    createNewNote,
+    deleteNote
+} = require('../../lib/noteFunctions');
+
 
 router.get('/notes', (req, res) => {
     let saved = notes;
     res.json(saved);
 })
+
 router.post('/notes', (req, res) => {
     req.body.id = notes.length.toString();
-    let note = newNote(req.body, notes);
+    let note = createNewNote(req.body, notes);
     res.json(note);
-});
+})
+
+router.delete('/notes/:id', (req, res) => {
+    deleteNote(notes, req.params.id);
+    res.json(notes);
+})
+
+
+module.exports = router;
